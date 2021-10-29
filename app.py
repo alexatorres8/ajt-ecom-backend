d8d48f5a420cd991ee0fa538a3aa00be153910fe
alexatorres8 <ajt8@live.com>
2021-10-29 12:44:49 -0400

    updated

--- app.py
+++ app.py
@@ -14,60 +14,19 @@ ma = Marshmallow(app)
 
 CORS(app)
 
-# User
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(20), nullable=False)
-
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
-
-class UserSchema(ma.Schema):
-    class Meta:
-        fields = ('email', 'password')
-
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
-
-# Endpoint to create a new user
-@app.route('/user', methods=["POST"])
-def add_user():
-    email = request.json['email']
-    password = request.json['password']
-
-    new_user = User(email, password)
-
-    db.session.add(new_user)
-    db.session.commit()
-
-    user = User.query.get(new_user.id)
-
-    return product_schema.jsonify(user)
-
-# Endpoint to query all user
-@app.route("/users", methods=["GET"])
-def get_user():
-    all_users = User.query.all()
-    result = users_schema.dump(all_users)
-    return jsonify(result)
-
-
-
 # Product
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    item = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Float, nullable=False)
 
-    def __init__(self, item, price):
-        self.item = item
+    def __init__(self, title, price):
+        self.title = title
         self.price = price
 
 class ProductSchema(ma.Schema):
     class Meta:
-        fields = ('item', 'price')
+        fields = ('title', 'price')
 
 product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
@@ -75,10 +34,10 @@ products_schema = ProductSchema(many=True)
 # Endpoint to create a new product
 @app.route('/product', methods=["POST"])
 def add_product():
-    item = request.json['item']
+    title = request.json['title']
     price = request.json['price']
 
-    new_product = Product(item, price)
+    new_product = Product(title, price)
 
     db.session.add(new_product)
     db.session.commit()
